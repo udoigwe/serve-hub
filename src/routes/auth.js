c77d961ecf215336229f08bfd0894b7347e48146
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2024-10-07 20:00:59 +0100

    feat: auth route

---
+++ src/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth.controller');
+const validators = require('../middleware/validators');
+const checkAuth = require('../middleware/checkAuth');
+
+router.post('/sign-up', validators.signUp, authController.signUp);
+router.post('/sign-in', validators.signIn, authController.signIn);
+router.put('/account-update', checkAuth.verifyToken, validators.updateAccount, authController.updateAccount);
+router.put('/password-update', checkAuth.verifyToken, validators.passwordUpdate, authController.updatePassword);
+
+module.exports = router;
